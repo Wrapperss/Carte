15703ac5289a1a81c46ebb644ddd25b689112701
Wrappers <zhanglive@outlook.com>
2018-04-08 16:02:05 +0800

    tab icon

--- Carte/Carte/Module/Main/Controller/MainTabBarController.swift
+++ Carte/Carte/Module/Main/Controller/MainTabBarController.swift
@@ -31,7 +31,7 @@ class MainTabBarController: UITabBarController {
 
         let vc1 = RNViewController(fileUrl: "Home/Page", initProps: nil).then {
             $0.title = "精选"
-            $0.tabBarItem = UITabBarItem(title: "精选", image: UIImage.init(named: "all"), selectedImage: nil)
+            $0.tabBarItem = UITabBarItem(title: "精选", image: UIImage(named: "home"), selectedImage: nil)
         }
         
 //        let vc1 = UIViewController().then {
@@ -41,27 +41,27 @@ class MainTabBarController: UITabBarController {
         let vc2 = UIViewController().then {
             $0.view.backgroundColor = UIColor.flatBlue
             $0.title = "分类"
+            $0.tabBarItem = UITabBarItem(title: "分类", image: UIImage(named: "classify"), selectedImage: nil)
         }
 
         let vc3 = UIViewController().then {
             $0.view.backgroundColor = UIColor.flatMint
             $0.title = "杂志"
+            $0.tabBarItem = UITabBarItem(title: "杂志", image: UIImage(named: "magazine"), selectedImage: nil)
         }
         
         let vc4 = CartController().then {
             $0.title = "购物车"
+            $0.tabBarItem = UITabBarItem(title: "购物车", image: UIImage(named: "cart"), selectedImage: nil)
         }
         
         mine.title = "我的"
+        mine.tabBarItem = UITabBarItem(title: "我的", image: UIImage(named: "mine"), selectedImage: nil)
         
         self.viewControllers = [MainNavigationController(rootViewController: vc1),
                                 MainNavigationController(rootViewController: vc2),
                                 MainNavigationController(rootViewController: vc3),
                                 MainNavigationController(rootViewController: vc4),
                                 MainNavigationController(rootViewController: mine)]
-        
-//        let tab1 = UITabBarItem(title: "精选", image: UIImage.init(named: "all"), selectedImage: nil)
-//
-//        self.tabBar.items = [tab1]
     }
 }
