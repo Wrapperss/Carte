7a94233004b66d1019b0eceb2dc35fee9b44f3f5
Wrappers <zhanglive@outlook.com>
2018-05-09 20:01:15 +0800

    fix

--- Carte-Server/app/service/OrderGoods.js
+++ Carte-Server/app/service/OrderGoods.js
@@ -4,14 +4,15 @@ const Service = require('egg').Service;
 
 class OrderGoodsService extends Service {
   async create(targets) {
-    const result = await this.app.mysql.insert('Order_Goods', targets)
+    const result = await this.app.mysql.insert('OrderGoods', targets)
     return result.insertId;
   }
 
   async findByOrderId(orderId) {
-    // const result = await this.app.mysql.query('SELECT * FROM Order_Goods WHERE orderId = ?' [orderId]);
-    let id = 16;
-    const result = await this.app.mysql.select('Order_Goods', { id })
+    const result = await this.app.mysql.query(`
+      select * 
+        from OrderGoods 
+          where OrderGoods.orderId = ?`, [orderId]);
     return result;
   }
 }
